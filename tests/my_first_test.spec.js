4cccb190d5d3398c3da84b4b344b30fd0717d24d
Saturn Gold Tycoon <289761640+saturngoldtycoon@users.noreply.github.com>
2026-07-23 08:51:31 -0400

    modified:   package.json 	modified:   tests/my_first_test.spec.js

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -1,7 +1,7 @@
-const {test, expect} = require('@playwright/test')
-// import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
+
 test('My first test', async ({page}) => {
 
-    await page.goto('https://google.com')
-    await (page).toHaveTitle('Google')
+    await page.goto('https://www.google.com')
+    await page.title('Google')
 })
