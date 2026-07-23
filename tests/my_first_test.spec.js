68f6da08061a306c5e428fbc73b909dc5a727fbd
Saturn Gold Tycoon <289761640+saturngoldtycoon@users.noreply.github.com>
2026-07-23 08:51:29 -0400

    modified:   tests/my_first_test.spec.js

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -1,5 +1,5 @@
 const {test, expect} = require('@playwright/test')
-const {hello, world} = require('./demo/hello.js'))
+const {hello, world} = require('./demo/hello.js')
 
 console.log (hello());
 console.log (world());
