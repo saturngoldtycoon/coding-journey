e619892529c0f78c618834558486b66a234f4237
Saturn Gold Tycoon <289761640+saturngoldtycoon@users.noreply.github.com>
2026-07-23 08:51:31 -0400

    modified:   tests/my_first_test.spec.js

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test')
 // import {test, expect} from '@playwright/test'
-test('My first test ', async ({page})=>{
+test('My first test', async ({page}) => {
 
     await page.goto('https://google.com')
     await (page).toHaveTitle('Google')
